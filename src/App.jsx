c451f93b07cde59f95e92de75eec34f590f9f7b5
N1khil <saininikhilnnn@gmail.com>
2023-09-21 15:57:16 +0530

    files uploaded

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+// import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      <Container />
+    </div>
+  );
+}
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <Logo />
+      <Menu />
+      <Button />
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <img src="/assets/img/Nike-Logo-500x281.webp" alt="nike-logo" />
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <ul className="nav-menu">
+      <li>Menu</li>
+      <li>Location</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  );
+}
+
+function Button() {
+  return <button className="login-btn">Login</button>;
+}
+
+function Container() {
+  return (
+    <div className="container">
+      <div className="text-container">
+        <div className="heading-container">
+          <h1>Your Feet Deserve the best</h1>
+        </div>
+        <div className="para-container">
+          <p>
+            Your Feet Deserve the best and we&apos;re here to help you with our
+            shoes. Your Feet Deserve the best and we&apos;re here to help you
+            with our shoes.
+          </p>
+        </div>
+        <div className="buttons-grp">
+          <button className="btn shop-now">Shop Now</button>
+          <button className="btn category">Category</button>
+        </div>
+        <Socials />
+      </div>
+      <div className="img-container">
+        <img src="/assets/img/hero-image.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+function Socials() {
+  return (
+    <div className="social">
+      <p>Also Availble On</p>
+      <ul>
+        <li>
+          <img src="/assets/img/flipkart.png" alt="flipkart-logo" />
+        </li>
+        <li>
+          <img src="/assets/img/amazon.png" alt="amazon-logo" />
+        </li>
+      </ul>
+    </div>
+  );
+}
